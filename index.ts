272531a3907b15229369710713194ab7a076a47b
olliolliallmond <aallmondcar@gmail.com>
2021-08-09 10:32:22 -0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,38 @@
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
+
+interface NewsItem {
+  category: 'Business' | 'Sports';
+  content: string;
+}
+
+const newsFeed$ = new Observable<NewsItme>(subscriber => {
+  setTimeout(
+    () => subscriber.next({ category: 'Business', content: 'A' }),
+    1000
+  );
+  setTimeout(() => subscriber.next({ category: 'Sports', content: 'B' }), 3000);
+  setTimeout(
+    () => subscriber.next({ category: 'Business', content: 'C' }),
+    4000
+  );
+  setTimeout(() => subscriber.next({ category: 'Sports', content: 'D' }), 6000);
+  setTimeout(
+    () => subscriber.next({ category: 'Business', content: 'E' }),
+    7000
+  );
+});
+
+// newsFeed$.subscribe(item => console.log(item));
+
+// newsFeed$
+//   .pipe(filter(item => item.category === 'Sports'))
+//   .subscribe(item => console.log(item));
+
+const sportsNewsFeed$ = newsFeed$.pipe(
+  filter(item => item.category === 'Sports')
+);
+
+sportsNewsFeed$.subscribe(item => console.log('Sports feed: ', item));
+
+newsFeed$.subscribe(item => console.log(item));
